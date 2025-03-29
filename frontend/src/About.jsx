cb3f023d30d056b839bc5f4e6c27057fe0c30f23
suryanshu2006 <singhsuryanshukumar5@gmail.com>
2025-03-29 11:02:11 +0530

    assignment

---
+++ frontend/src/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>Samosa Surprises</h1>
+      <p>
+        Welcome to Samosa Surprises! We explore the world of unique and unusual
+        samosa stuffings to redefine your snacking experience. From fusion
+        flavors to sweet treats, there's something here to delight every
+        adventurous foodie.
+      </p>
+      <h2>Highlights:</h2>
+      <ul>
+        <li>Fusion Flavors: Mozzarella & marinara, spicy beans & jalapeños.</li>
+        <li>Sweet Tooth Delights: Chocolate & marshmallows, caramelized bananas.</li>
+        <li>Healthy Alternatives: Quinoa & spinach, roasted vegetables & feta.</li>
+        <li>Regional Explorations: Andhra spicy masala, Kolkata-style fish stuffing.</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default About
